cbd8ce0a9f5c59c1670821bc96b075d4e8c2ffce
toshi201 <toshi.stce@gmail.com>
2023-12-29 00:18:55 +0900

    add comment

--- rust/combination_mod.rs
+++ rust/combination_mod.rs
@@ -1,9 +1,12 @@
 use ac_library::ModInt1000000007 as Mint;
 
+// N!
 fn fact_mod(N: i64) -> Mint {
     (1..=N).fold(Mint::new(1), |sum, x| sum*x)
 }
 
+// nCr
+// n! / (r!*(n - r)!)
 fn comb_mod(n: i64, r: i64) -> Mint {
     fact_mod(n) / (fact_mod(r) * fact_mod(n - r))
 }
